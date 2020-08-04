3aa288c99a299d0ffb3ef8284f71daa9f5789fbc
Quintin Russ <q@Quintins-MacBook-Pro.local>
2020-08-04 20:06:36 +1200

    commiting file

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 
 // Hello /serverless/NIGHTS - Auckland
 
-/*use SilverStripe\Control\HTTPApplication;
+use SilverStripe\Control\HTTPApplication;
 use SilverStripe\Control\HTTPRequestBuilder;
 use SilverStripe\Core\CoreKernel;
 use SilverStripe\Core\Startup\ErrorControlChainMiddleware;
@@ -17,6 +17,6 @@ $kernel = new CoreKernel(BASE_PATH);
 $app = new HTTPApplication($kernel);
 $app->addMiddleware(new ErrorControlChainMiddleware($app));
 $response = $app->handle($request);
-$response->output();*/
+$response->output();
 
-phpinfo();
+//phpinfo();
